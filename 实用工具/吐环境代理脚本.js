22492d888ab0503d63af0707a67d3e6741291469
xiazhimiao <3309686397@qq.com>
2026-07-10 19:23:52 +0800

    新增实用工具目录 - 吐环境代理脚本与README

---
+++ 实用工具/吐环境代理脚本.js
@@ -0,0 +1,26 @@
+function getEnv(proxy_array) {
+    for (let i = 0; i < proxy_array.length; i++) {
+        handler = `{
+            get: function(target, property, receiver) {
+                   console.log('方法：get','    对象：${proxy_array[i]}','    属性：',property,'    属性类型：',typeof property,'    属性值类型：',typeof target[property]);
+                   return target[property];
+            },
+            set: function(target, property, value, receiver){
+                    console.log('方法：set','    对象：${proxy_array[i]}','    属性：',property,'    属性类型：',typeof property,'    属性值类型：',typeof target[property]);
+                    return Reflect.set(...arguments);
+            }
+        }`;
+        eval(`
+            try {
+                ${proxy_array[i]};
+                ${proxy_array[i]} = new Proxy(${proxy_array[i]}, ${handler});
+            } catch (e) {
+                ${proxy_array[i]} = {};
+                ${proxy_array[i]} = new Proxy(${proxy_array[i]}, ${handler});
+            }
+        `);
+    }
+}
+
+proxy_array = ['window', 'document', 'location', 'navigator', 'history', 'screen']
+getEnv(proxy_array);
